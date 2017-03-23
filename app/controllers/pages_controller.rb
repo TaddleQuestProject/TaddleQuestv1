bc85715ab482eb8816c549598539ce5e891c6532
mibautista <mibautista@gbox.adnu.edu.ph>
2017-03-23 12:33:01 +0800

    Add files via upload

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def page1
+    
+  end
+
+
+  def page2
+  end
+
+  def page3
+  
+  end
+
+  def page4
+    @weapons = Weapon.all
+
+  end
+  def page44
+    @weapons = Weapon.all
+
+  end
+  def victory
+    
+  end
+  def defeat
+    
+  end
+  def inventory
+  end
+
+  def status
+  	
+  end
+end
